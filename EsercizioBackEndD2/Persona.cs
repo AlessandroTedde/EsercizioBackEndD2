58058c68ec6391409878bdad5da2fb4696b7540f
AlessandroTedde <148778362+AlessandroTedde@users.noreply.github.com>
2024-01-30 15:20:57 +0100

    Aggiungere i file di progetto.

---
+++ EsercizioBackEndD2/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioBackEndD2
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Età { get; set; }
+
+        public string GetNome() 
+        { 
+            return Nome; 
+        }
+
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+
+        public int GetEtà()
+        {
+            return Età;
+        }
+        public void GetDettagli()
+        {
+            Console.WriteLine("Dettagli persona:" + "\n" + "Nome: " + GetNome() + "\n" + "Cognome: " + GetCognome() + "\n" + "Età: " + Convert.ToString(GetEtà()));
+        }
+        public Persona(string nome, string cognome, int età)
+        {
+            this.Nome = nome;
+            this.Cognome = cognome;
+            this.Età = età;
+        }
+    }
+}
---
+++ EsercizioBackEndD2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioBackEndD2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci Nome:");
+            string Nome = Console.ReadLine(); 
+            Console.WriteLine("Inserisci Cognome:");
+            string Cognome = Console.ReadLine();            
+            Console.WriteLine("Inserisci Età:");
+            int Età = Convert.ToInt16(Console.ReadLine());
+
+            Persona persona = new Persona(Nome, Cognome, Età);
+            Console.WriteLine("\n");
+            persona.GetDettagli();
+            Console.ReadKey();
+        }
+    }
+}
